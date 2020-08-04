90ee8bc535617a236a2f57830b2081482b3d9f37
CyrusTheCurator <joshua.c.luscombe@gmail.com>
2020-08-04 14:41:25 -0700

    search fix

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -29,36 +29,25 @@ selecto_perfecto = selection_sort([8,3,6,7,10,10,10,99,99,300])
 print("Selection Sort: ",selecto_perfecto)
 
 # TO-DO:  implement the Bubble Sort function below
-def bubble_sort(arr):
-    # Your code here
-
-
-    return arr
-
-
-
-def bubbleSort(arr): 
+def bubble_sort(arr): 
     n = len(arr) 
-  
     # Traverse through all array elements 
     for i in range(n-1): 
     # range(n) also work but outer loop will repeat one time more than needed. 
   
         # Last i elements are already in place 
         for j in range(0, n-i-1): 
-  
             # traverse the array from 0 to n-i-1 
             # Swap if the element found is greater 
             # than the next element 
             if arr[j] > arr[j+1] : 
                 arr[j], arr[j+1] = arr[j+1], arr[j]
-
     return arr 
   
 # Driver code to test above 
-arr = [64, 34, 25, 12, 22, 11, 2] 
+arr = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7] 
   
-zee = bubbleSort(arr) 
+zee = bubble_sort(arr) 
   
 print ("Bubble Sort: ",zee) 
 
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,30 @@
 def linear_search(arr, target):
     # Your code here
-
+    for i, elem in enumerate(arr):
+        if elem==target:
+            return i
 
     return -1   # not found
-
+# print(linear_search([3,5,6], 8))
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    low = 0
+    high = len(arr)-1
 
+    while low <= high:
+        middle = (low+high)//2
+        guess = arr[middle]
+        if guess == target:
+            return middle
+        elif guess >= target:
+            high = middle - 1
+        elif guess <= target:
+            low = middle + 1
     # Your code here
+    
 
 
     return -1  # not found
+
+
